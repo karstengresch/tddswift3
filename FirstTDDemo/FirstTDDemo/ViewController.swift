d95956f4b05743982fcf03539c94723d41016ddb
Karsten Gresch <git-2015@by-doing.org>
2017-02-11 22:42:51 +0100

    FirstTDDemo init.

---
+++ FirstTDDemo/FirstTDDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstTDDemo
+//
+//  Created by Karsten Gresch on 11.02.17.
+//  Copyright © 2017 Closure One. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
