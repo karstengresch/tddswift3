98445e49facc2738965ec11959cd1ab6c3ad8862
Karsten Gresch <git-2015@by-doing.org>
2017-02-23 22:25:11 +0100

    Method skeleton. Dunno why the return type wasn't in the example

--- FirstTDDemo/FirstTDDemo/ViewController.swift
+++ FirstTDDemo/FirstTDDemo/ViewController.swift
@@ -31,6 +31,10 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
   
+  func makeHeadline(from string: String) -> String {
+    
+  }
+  
  
 
 }
